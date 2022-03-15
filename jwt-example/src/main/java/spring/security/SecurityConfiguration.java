50571a5e5b63ac53cb554ed5f02d6435cbb4f42e
Anès MAHI <fsociety@fsocietyhq.home>
2022-03-16 00:04:34 +0100

    organise imports

--- jwt-example/src/main/java/spring/security/SecurityConfiguration.java
+++ jwt-example/src/main/java/spring/security/SecurityConfiguration.java
@@ -1,11 +1,9 @@
 package spring.security;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
